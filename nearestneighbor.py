33cb0ecede745fc6544393fa3d304f1415cab247
Videogamer80 <karsten.keiji@gmail.com>
2026-03-23 20:24:08 -0700

    KNN final commit from Karsten, just need to be checked by everyone else

--- nearestneighbor.py
+++ nearestneighbor.py
@@ -31,17 +31,56 @@ def load_unknown_data(file_path):
 
     return unknown_rows
 
-
 def main():
+    #Section that reads the file, written by Akshay, including all the methods above
     train_file = "MushroomData_8000.txt"
     unknown_file = "MushroomData_Unknwon_100.txt"
 
     train_rows, train_labels, train_features = load_training_data(train_file)
     unknown_rows = load_unknown_data(unknown_file)
 
-    print("Train rows:", len(train_rows))
-    print("Unknown rows:", len(unknown_rows))
+    #Section that calculates the cartesian distance and the probability that the unknown is edible, written by Karsten
 
+    probabilities = []
+
+    for unknownItems in range(len(unknown_rows)):
+        distances = []
+
+        for knownItems in range(len(train_rows)):
+            distance = 0
+
+            for field in range(0, 22):
+                if unknown_rows[unknownItems][field] != train_rows[knownItems][field + 1]:
+                    distance += 1
+
+            distances.append(distance)
+
+        lowestDistance = 9999
+        lowestIndexes = []
+        for i in range(len(distances)):
+            if distances[i] < lowestDistance:
+                lowestDistance = distances[i]
+                lowestIndexes.clear()
+                lowestIndexes.append(i)
+            elif distances[i] == lowestDistance:
+                lowestIndexes.append(i)
+
+        probability = 0.0
+        for i in range(len(lowestIndexes)):
+            if train_rows[i][0] == "e":
+                probability += 1
+
+        probability /= len(lowestIndexes)
+        probability *= 100
+        # print(probability)
+
+        probabilities.append(probability)
+
+    for i in range(len(probabilities)):
+        print("%.2f" % probabilities[i], end = "")
+        print("% of unknown mushroom #", end= "")
+        print(i+1, end= "")
+        print(" being edible")
 
 if __name__ == "__main__":
     main()
